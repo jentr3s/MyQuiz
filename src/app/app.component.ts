0ed0d90ad9f9183a9edfbc47e7a7469f74fdffa9
jentr3s <jentr3s03@gmail.com>
2018-03-24 11:56:55 +0800

    Setup electron with angular5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'MyQuiz';
+  
+  constructor(private _electronService: ElectronService) {}   // DI
+  
+  launchWindow() {
+    this._electronService.shell.openExternal('https://youtube.com');
+  }
 }
