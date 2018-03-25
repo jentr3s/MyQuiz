31efed72ddcd45e3576ad2915d3898361aa52c56
jentr3s <jentr3s03@gmail.com>
2018-03-25 14:56:30 +0800

    connected with database

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,10 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('./database/MyQuiz');
+var db = new sqlite3.Database('./database/test');
 
 db.serialize(function() {
-    db.run("CREATE TABLE [IF NOT EXISTS] user (info TEXT)");
+    db.run("CREATE TABLE lorem (info TEXT)");
 
-    var stmt = db.prepare("INSERT INTO user VALUES (?)");
+    var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
     for (var i = 0; i < 10; i++) {
         stmt.run("Ipsum " + i);
     }
